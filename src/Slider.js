a7c50c0105b6bfa4f4bb175cb3927b6b7c8be641
Alikhan <58951356+Alex228-usa@users.noreply.github.com>
2023-10-27 16:08:21 +0600

    Create Slider.js

---
+++ src/Slider.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { interval, from } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
+import axios from 'axios';
+
+const Slider = ({ displayTime, slidesToShow }) => {
+  // ... The rest of your Slider component code
+};
+
+export default Slider;
